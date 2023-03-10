c00ad224b1d90c8029b1c459ac40b56e8923fe24
bga32 <bga32@sfu.ca>
2023-03-10 14:51:27 -0800

    food truck image

--- foodtruckfrenzy/src/main/java/foodtruckfrenzy/Drawable.java
+++ foodtruckfrenzy/src/main/java/foodtruckfrenzy/Drawable.java
@@ -1,7 +1,7 @@
 package foodtruckfrenzy;
 
-import java.awt.Graphics2D;
-import java.awt.Color;
+import javax.swing.*;
+import java.awt.*;
 
 public abstract class Drawable {
 
@@ -9,11 +9,21 @@ public abstract class Drawable {
     int _col;
     int _row;
 
+    private Image image;
+
+    public Drawable() {
+        image = new ImageIcon("src/main/resources/foodtruckfrenzy/foodtruck.png").getImage();
+    }
+
     public abstract Color getGraphic();
 
     public void draw(Graphics2D g2d, int cellSize) {
         g2d.setColor(this.getGraphic());
         g2d.fillRect(_col * cellSize, _row * cellSize, cellSize, cellSize);
+    }
+
+    public void drawImage(Graphics2D g2d, int cellSize) {
+        g2d.drawImage(image, _col * cellSize, _row * cellSize, cellSize, cellSize, null);
     }
 
 }
