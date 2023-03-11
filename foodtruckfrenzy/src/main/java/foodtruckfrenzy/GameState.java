09f310b0d48f0dad8684e0e20a9c70d692064d8f
cpinca <cpinca@sfu.ca>
2023-03-10 17:32:26 -0800

    Added commented out code to be added to GameLoop to keep track of current GameState

--- foodtruckfrenzy/src/main/java/foodtruckfrenzy/GameState.java
+++ foodtruckfrenzy/src/main/java/foodtruckfrenzy/GameState.java
@@ -16,3 +16,20 @@ public class GameState {
         return state;
     }
 }
+
+/* ADD THIS TO GAMELOOP TO KEEP TRACK OF GAMESTATE
+
+GameState gameState = new GameState();
+
+while (true) {
+    if (gameState.getState() == GameState.PLAY_STATE) {
+        // Update game logic and render the game
+    } else if (gameState.getState() == GameState.PAUSE_STATE) {
+        // Show pause screen and wait for user input
+    } else if (gameState.getState() == GameState.GAMEOVER_STATE) {
+        // Show gameover screen and wait for user input
+    }
+}
+
+how to set state: gameState.setState(GameState.PauseState);
+ */
