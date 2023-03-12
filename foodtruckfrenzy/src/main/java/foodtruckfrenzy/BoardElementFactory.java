cb3dc1b7df3855d9bd37898146a3476038d640a0
Kyle Deliyannides <kkd16@sfu.ca>
2023-03-12 14:12:13 -0700

    UPDATE BOARD ELEMENT FACTORY FOR ALL ROAD

--- foodtruckfrenzy/src/main/java/foodtruckfrenzy/BoardElementFactory.java
+++ foodtruckfrenzy/src/main/java/foodtruckfrenzy/BoardElementFactory.java
@@ -9,6 +9,26 @@ public class BoardElementFactory {
                 return new Road(row, col, null, DrawableEnum.VERTICAL_ROAD);
             case X:
                 return new Road(row, col, null, DrawableEnum.FOUR_WAY_INTERSECT);
+
+            case U:
+                return new Road(row, col, null, DrawableEnum.NORTH_WEST_CORNER);
+            case I:
+                return new Road(row, col, null, DrawableEnum.NORTH_EAST_CORNER);
+            case J:
+                return new Road(row, col, null, DrawableEnum.SOUTH_EAST_CORNER);
+            case K:
+                return new Road(row, col, null, DrawableEnum.SOUTH_WEST_CORNER);
+                
+            case N:
+                return new Road(row, col, null, DrawableEnum.THREE_WAY_NORTH);
+            case B:
+                return new Road(row, col, null, DrawableEnum.THREE_WAY_EAST);
+            case M:
+                return new Road(row, col, null, DrawableEnum.THREE_WAY_SOUTH);
+            case C:
+                return new Road(row, col, null, DrawableEnum.THREE_WAY_WEST);
+
+
             case O:
                 return new Obstruction(row, col);
             case S:
