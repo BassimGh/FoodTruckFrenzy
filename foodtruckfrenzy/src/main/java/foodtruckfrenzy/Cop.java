b0cb947b077ab6681d1cf771d6899f892c78b29d
bga32 <bga32@sfu.ca>
2023-03-12 00:13:49 -0800

    cop following logic (incomplete)

--- foodtruckfrenzy/src/main/java/foodtruckfrenzy/Cop.java
+++ foodtruckfrenzy/src/main/java/foodtruckfrenzy/Cop.java
@@ -6,10 +6,13 @@ public class Cop extends Vehicle{
 
     private FoodTruck foodtruck;
 
-    private ArrayList<Direction> directions = new ArrayList<>();
+    private int targetX;
+    private int targetY;
 
     public Cop(int row, int col, Grid grid, FoodTruck target) {
         super(row, col, grid, DrawableEnum.COP_RIGHT);
+        targetX = target.getCol();
+        targetY = target.getRow();
     }
 
     @Override
@@ -42,16 +45,60 @@ public class Cop extends Vehicle{
 
     public void chaseTruck() {
         this.moveRight();
-        // int currentX = this.getRow();
-        // int currentY = this.getCol();
+        int currentX = this.getRow();
+        int currentY = this.getCol();
 
-        // int targetX = foodtruck.getCol();
-        // int targetY = foodtruck.getRow();
+        int targetX = foodtruck.getCol();
+        int targetY = foodtruck.getRow();
+
+        getPath();
 
         // while (!(currentX == targetX && currentY == targetY)) {
-            
+        //     move();
         // }
-        // getPath();
+    }
+
+    public void getPath() {
+        Position currentPosition = new Position(this.getCol(), this.getRow());
+        ArrayList<Direction> allDirections = new ArrayList<>();
+        allDirections.add(Direction.UP);
+        allDirections.add(Direction.DOWN);
+        allDirections.add(Direction.LEFT);
+        allDirections.add(Direction.RIGHT);
+
+        ArrayList<Position> visited = new ArrayList<>();
+        ArrayList<Position> toVisit = new ArrayList<>(); 
+
+        toVisit.add(currentPosition);
+        visited.add(currentPosition);
+
+        while (!allDirections.isEmpty()) {
+            if (isValidMove(allDirections.get(0))) {
+                toVisit.add()
+            }
+            allDirections.remove(0);
+        }
+
+    }
+
+    public Direction isValidMove(Direction direction) {
+        if (direction == Direction.UP && targetY - 1 > 0 &&
+            this.getGrid().getCell(targetX, targetY - 1).isObstruction())
+                return Direction.UP;
+
+        if (direction == Direction.DOWN && targetY + 1 < Grid.ROWS &&
+            this.getGrid().getCell(targetX, targetY + 1).isObstruction())
+                return Direction.DOWN;
+
+        if (direction == Direction.LEFT && targetX - 1 > 0 &&
+            this.getGrid().getCell(targetX - 1, targetY).isObstruction())
+                return Direction.LEFT;
+
+        if (direction == Direction.RIGHT && targetX + 1 < Grid.COLS &&
+            this.getGrid().getCell(targetX + 1, targetY).isObstruction())
+                return Direction.RIGHT;
+
+        return null;
     }
 
 }
