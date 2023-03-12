44fd688a0f0a15ed1fa46c087fcaf53e83d5bdc3
Kyle Deliyannides <kkd16@sfu.ca>
2023-03-12 00:12:48 -0800

    add 2 more houses

--- foodtruckfrenzy/src/main/java/foodtruckfrenzy/SpriteLoader.java
+++ foodtruckfrenzy/src/main/java/foodtruckfrenzy/SpriteLoader.java
@@ -3,6 +3,8 @@ package foodtruckfrenzy;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class SpriteLoader {
 
@@ -15,11 +17,14 @@ public class SpriteLoader {
     private static Image _copLeftImage;
     private static Image _copRightImage;
     private static Image _roadImage;
-    private static Image _obstructionImage;
     private static Image _speedTrapImage;
     private static Image _potHoleImage;
     private static Image _foodImage;
 
+    private static ArrayList<Image> _houseImages = new ArrayList<Image>();
+
+    private static Random random = new Random();
+    
     static {
             
         try {
@@ -34,10 +39,13 @@ public class SpriteLoader {
             InputStream copRightStream = SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/copCar.png");
             
             InputStream roadStream = SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/road.png");
-            InputStream obstructionStream = SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/house.png");
             InputStream speedTrapStream = SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/speedtrap.png");
             InputStream potHoleStream = SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/pothole.png");
             InputStream foodStream = SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/food.png");
+
+            InputStream house1Stream = SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/house1.png");
+            InputStream house2Stream = SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/house2.png");
+            InputStream house3Stream = SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/house3.png");
 
             _foodTruckUpImage = ImageIO.read(foodTruckUpStream);
             _foodTruckDownImage = ImageIO.read(foodTruckDownStream);
@@ -50,10 +58,14 @@ public class SpriteLoader {
             _copRightImage = ImageIO.read(copRightStream);
 
             _roadImage = ImageIO.read(roadStream);
-            _obstructionImage = ImageIO.read(obstructionStream);
             _speedTrapImage = ImageIO.read(speedTrapStream);
             _potHoleImage = ImageIO.read(potHoleStream);
             _foodImage = ImageIO.read(foodStream);
+
+            _houseImages.add(ImageIO.read(house1Stream));
+            _houseImages.add(ImageIO.read(house2Stream));
+            _houseImages.add(ImageIO.read(house3Stream));
+
 
         } catch (Exception e) {
             System.err.println("Error when loading sprite files with SpriteLoader:");
@@ -83,7 +95,8 @@ public class SpriteLoader {
             case ROAD:
                 return _roadImage;
             case OBSTRUCTION:
-                return _obstructionImage;
+                int randomNumber = random.nextInt(3);
+                return _houseImages.get(randomNumber);
             case SPEED_TRAP:
                 return _speedTrapImage;
             case POT_HOLE:
