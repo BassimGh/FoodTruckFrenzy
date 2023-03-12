a3b6c6f12f86daaf5f1c74d7f4d46c298765755a
Kyle Deliyannides <kkd16@sfu.ca>
2023-03-12 14:04:49 -0700

    add all the image files

--- foodtruckfrenzy/src/main/java/foodtruckfrenzy/BoardElementFactory.java
+++ foodtruckfrenzy/src/main/java/foodtruckfrenzy/BoardElementFactory.java
@@ -4,15 +4,19 @@ public class BoardElementFactory {
     public static BoardElement create(BoardElementEnum type, int row, int col) {
         switch (type) {
             case H:
-                return new Road(row, col, null);
+                return new Road(row, col, null, DrawableEnum.HORIZONTAL_ROAD);
+            case V:
+                return new Road(row, col, null, DrawableEnum.VERTICAL_ROAD);
+            case X:
+                return new Road(row, col, null, DrawableEnum.FOUR_WAY_INTERSECT);
             case O:
                 return new Obstruction(row, col);
             case S:
-                return new Road(row, col, new SpeedTrap(row, col));
+                return new Road(row, col, new SpeedTrap(row, col), DrawableEnum.HORIZONTAL_ROAD);
             case P:
-                return new Road(row, col, new PotHole(row, col));
+                return new Road(row, col, new PotHole(row, col), DrawableEnum.HORIZONTAL_ROAD);
             case F:
-                return new Road(row, col, new Food(row, col));
+                return new Road(row, col, new Food(row, col), DrawableEnum.HORIZONTAL_ROAD);
             default:
                 throw new IllegalArgumentException();
         }
--- foodtruckfrenzy/src/main/java/foodtruckfrenzy/SpriteLoader.java
+++ foodtruckfrenzy/src/main/java/foodtruckfrenzy/SpriteLoader.java
@@ -17,46 +17,28 @@ public class SpriteLoader {
     static {
             
         try {
-            InputStream foodTruckUpStream = SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/foodtruckUp.png");
-            InputStream foodTruckDownStream = SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/foodtruckDown.png");
-            InputStream foodTruckLeftStream = SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/foodtruckLeft.png");
-            InputStream foodTruckRightStream = SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/foodtruckRight.png");
+
+            _imagesHashMap.put(DrawableEnum.FOODTRUCK_UP, ImageIO.read(SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/foodtruckUp.png")));
+            _imagesHashMap.put(DrawableEnum.FOODTRUCK_DOWN, ImageIO.read(SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/foodtruckDown.png")));
+            _imagesHashMap.put(DrawableEnum.FOODTRUCK_RIGHT, ImageIO.read(SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/foodtruckRight.png")));
+            _imagesHashMap.put(DrawableEnum.FOODTRUCK_LEFT, ImageIO.read(SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/foodtruckLeft.png")));
             
-            InputStream copUpStream = SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/copCar.png");
-            InputStream copDownStream = SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/copCar.png");
-            InputStream copLeftStream = SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/copCar.png");
-            InputStream copRightStream = SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/copCar.png");
-            
-            InputStream speedTrapStream = SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/speedtrap.png");
-            InputStream potHoleStream = SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/pothole.png");
-            InputStream foodStream = SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/food.png");
+            _imagesHashMap.put(DrawableEnum.COP_UP, ImageIO.read(SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/copCar.png")));
+            _imagesHashMap.put(DrawableEnum.COP_DOWN, ImageIO.read(SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/copCar.png")));
+            _imagesHashMap.put(DrawableEnum.COP_RIGHT, ImageIO.read(SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/copCar.png")));
+            _imagesHashMap.put(DrawableEnum.COP_LEFT, ImageIO.read(SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/copCar.png")));
 
-            InputStream house1Stream = SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/house1.png");
-            InputStream house2Stream = SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/house2.png");
-            InputStream house3Stream = SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/house3.png");
+            _imagesHashMap.put(DrawableEnum.SPEED_TRAP, ImageIO.read(SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/speedtrap.png")));
+            _imagesHashMap.put(DrawableEnum.POT_HOLE, ImageIO.read(SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/pothole.png")));
+            _imagesHashMap.put(DrawableEnum.FOOD, ImageIO.read(SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/food.png")));
 
-            InputStream roadHorizontalStream = SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/roadHorizontal.png");
+            _houseImages.add(ImageIO.read(SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/house1.png")));
+            _houseImages.add(ImageIO.read(SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/house2.png")));
+            _houseImages.add(ImageIO.read(SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/house3.png")));
 
-
-            _imagesHashMap.put(DrawableEnum.FOODTRUCK_UP, ImageIO.read(foodTruckUpStream));
-            _imagesHashMap.put(DrawableEnum.FOODTRUCK_DOWN, ImageIO.read(foodTruckDownStream));
-            _imagesHashMap.put(DrawableEnum.FOODTRUCK_RIGHT, ImageIO.read(foodTruckRightStream));
-            _imagesHashMap.put(DrawableEnum.FOODTRUCK_LEFT, ImageIO.read(foodTruckLeftStream));
-            
-            _imagesHashMap.put(DrawableEnum.COP_UP, ImageIO.read(copUpStream));
-            _imagesHashMap.put(DrawableEnum.COP_DOWN, ImageIO.read(copDownStream));
-            _imagesHashMap.put(DrawableEnum.COP_RIGHT, ImageIO.read(copRightStream));
-            _imagesHashMap.put(DrawableEnum.COP_LEFT, ImageIO.read(copLeftStream));
-
-            _imagesHashMap.put(DrawableEnum.SPEED_TRAP, ImageIO.read(speedTrapStream));
-            _imagesHashMap.put(DrawableEnum.POT_HOLE, ImageIO.read(potHoleStream));
-            _imagesHashMap.put(DrawableEnum.FOOD, ImageIO.read(foodStream));
-
-            _imagesHashMap.put(DrawableEnum.HORIZONTAL_ROAD, ImageIO.read(roadHorizontalStream));
-
-            _houseImages.add(ImageIO.read(house1Stream));
-            _houseImages.add(ImageIO.read(house2Stream));
-            _houseImages.add(ImageIO.read(house3Stream));
+            _imagesHashMap.put(DrawableEnum.HORIZONTAL_ROAD, ImageIO.read(SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/roadHorizontal.png")));
+            _imagesHashMap.put(DrawableEnum.VERTICAL_ROAD, ImageIO.read(SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/roadVertical.png")));
+            _imagesHashMap.put(DrawableEnum.FOUR_WAY_INTERSECT, ImageIO.read(SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/roadFourWay.png")));
 
 
         } catch (Exception e) {
