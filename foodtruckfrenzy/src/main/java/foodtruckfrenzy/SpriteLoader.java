2d24215cfa6661d7ef9af6e103d0805dd5d5dcde
Kyle Deliyannides <kkd16@sfu.ca>
2023-03-11 18:10:09 -0800

    update sprite loader to catch errors

--- foodtruckfrenzy/src/main/java/foodtruckfrenzy/SpriteLoader.java
+++ foodtruckfrenzy/src/main/java/foodtruckfrenzy/SpriteLoader.java
@@ -14,14 +14,13 @@ public class SpriteLoader {
     private static Image _potHoleImage;
 
     static {
-
-        InputStream foodTruckStream = SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/foodtruck.png");
-        InputStream roadStream = SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/road.png");
-        InputStream obstructionStream = SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/house.png");
-        InputStream speedTrapStream = SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/speedtrap.png");
-        InputStream potHoleStream = SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/pothole.png");
-
         try {
+
+            InputStream foodTruckStream = SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/foodtruck.png");
+            InputStream roadStream = SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/road.png");
+            InputStream obstructionStream = SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/house.png");
+            InputStream speedTrapStream = SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/speedtrap.png");
+            InputStream potHoleStream = SpriteLoader.class.getResourceAsStream("/foodtruckfrenzy/pothole.png");
             _foodTruckImage = ImageIO.read(foodTruckStream);
             _roadImage = ImageIO.read(roadStream);
             _obstructionImage = ImageIO.read(obstructionStream);
@@ -29,6 +28,7 @@ public class SpriteLoader {
             _potHoleImage = ImageIO.read(potHoleStream);
 
         } catch (IOException e) {
+            System.err.println("Error when loading sprite files with SpriteLoader:");
             e.printStackTrace();
         }  
     }
