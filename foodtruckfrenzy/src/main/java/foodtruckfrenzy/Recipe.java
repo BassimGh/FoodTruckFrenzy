7f24a6b656a8aa1c10e92eddad4cb45a40d80501
Patrick Shaw <patrickkshaw@gmail.com>
2023-03-12 20:39:15 -0700

    added count methods for recipe

--- foodtruckfrenzy/src/main/java/foodtruckfrenzy/Recipe.java
+++ foodtruckfrenzy/src/main/java/foodtruckfrenzy/Recipe.java
@@ -1,9 +1,18 @@
 package foodtruckfrenzy;
 
 public class Recipe extends Item {
-
+    public static int _count = 0;
     public Recipe(int row, int col) {
         super(row, col, DrawableEnum.RECIPE, 1000, ScoreType.BONUS);
+        Recipe._count++;
+    }
+
+    public static int getCount() {
+        return _count;
+    }
+
+    public static void resetCount() {
+        _count = 0;
     }
     
 }
