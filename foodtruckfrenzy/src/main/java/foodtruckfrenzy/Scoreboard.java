c80048d673f8438d38a95674c0063df04413df40
Patrick Shaw <patrickkshaw@gmail.com>
2023-03-12 22:05:26 -0700

    paused timer fix - scoreboard

--- foodtruckfrenzy/src/main/java/foodtruckfrenzy/Scoreboard.java
+++ foodtruckfrenzy/src/main/java/foodtruckfrenzy/Scoreboard.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionListener;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.FlowLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.Timer;
@@ -23,12 +24,15 @@ public class Scoreboard extends JPanel {
     4. Time:                   records time elapsed
     5. Score:                  current score
     **/
-    Font font = new Font("Arial", Font.BOLD, 18);
+    Font infoFont = new Font("Arial", Font.BOLD, 18);
+    Font damageFont = new Font("Arial", Font.BOLD, 18);
+    
     Color backGroundColor = new Color(54, 65, 79);
     
     private JPanel collectablesPanel = new JPanel();
-    private JPanel scorePanel = new JPanel();
     private JPanel negativePointsPanel = new JPanel();
+    private JPanel scorePanel = new JPanel();
+    private JPanel clockPanel = new JPanel();
     
     private JLabel _scoreLabel = new JLabel("Score: ");
     private JLabel _scoreTotal = new JLabel();
@@ -38,9 +42,13 @@ public class Scoreboard extends JPanel {
     private JLabel _fineLabel = new JLabel("Fines: ");
     private JLabel _timeLabel = new JLabel();
 
+    // private JLabel _pauseInstructions = new JLabel("Press 'P' to pause");
+
     private Timer _timer;
     private int _minute;
     private int _second; 
+
+    private boolean _paused = false;
     
 
     private FoodTruck player;
@@ -50,42 +58,51 @@ public class Scoreboard extends JPanel {
     public Scoreboard(FoodTruck player) {
         
 
-        setLayout(new GridLayout(1, 4));
+        setLayout(new FlowLayout());
 
         collectablesPanel.setLayout(new GridLayout(2, 1));
         collectablesPanel.setBackground(backGroundColor);
-        _ingredientsLabel.setFont(font);
+        _ingredientsLabel.setFont(infoFont);
         _ingredientsLabel.setForeground(Color.WHITE);
-        _recipesLabel.setFont(font);
+        _recipesLabel.setFont(infoFont);
         _recipesLabel.setForeground(Color.WHITE);
         collectablesPanel.add(_ingredientsLabel);
         collectablesPanel.add(_recipesLabel);
 
-        scorePanel.setLayout(new GridLayout(2, 1));
+        scorePanel.setLayout(new FlowLayout());
         scorePanel.setBackground(backGroundColor);
-        _scoreLabel.setFont(font);       
+        _scoreLabel.setFont(infoFont);       
         _scoreLabel.setForeground(Color.WHITE);
-        _scoreTotal.setFont(font);
+        _scoreTotal.setFont(infoFont);
         _scoreTotal.setForeground(new Color(241, 215, 77)); // yellow
         scorePanel.add(_scoreLabel);
         scorePanel.add(_scoreTotal);
 
         negativePointsPanel.setLayout(new GridLayout(2, 1));
         negativePointsPanel.setBackground(backGroundColor);
-        _fineLabel.setFont(font);
+        _fineLabel.setFont(infoFont);
         _fineLabel.setForeground(Color.WHITE);
-        _timeLabel.setFont(font);
+        _timeLabel.setFont(infoFont);
         _timeLabel.setForeground(Color.WHITE); 
-        _damageLabel.setFont(font);
+        _damageLabel.setFont(damageFont);
         _damageLabel.setForeground(Color.WHITE);
         negativePointsPanel.add(_fineLabel);
         negativePointsPanel.add(_damageLabel);
+
+        clockPanel.setLayout(new FlowLayout());
+        clockPanel.setBackground(backGroundColor);
+        _timeLabel.setFont(infoFont);
+        _timeLabel.setForeground(Color.RED);
+        // _pauseInstructions.setFont(font);
+        // _pauseInstructions.setForeground(Color.WHITE);
+        clockPanel.add(_timeLabel);
+        // clockPanel.add(_pauseInstructions);
         
 
         add(collectablesPanel);
-        add(scorePanel);
         add(negativePointsPanel);
-        add(_timeLabel);
+        add(scorePanel);
+        add(clockPanel);
 
         
         setBackground(backGroundColor);
@@ -112,14 +129,33 @@ public class Scoreboard extends JPanel {
         _timer = new Timer(1000, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                if (!_paused) {
                 _second++;
-                if (_second == 60) {
-                    _minute++;
-                    _second = 0;
+                    if (_second == 60) {
+                        _minute++;
+                        _second = 0;
+                    }
+                    _timeLabel.setText("Time: " + String.format("%02d",_minute) + ":" + String.format("%02d", _second));
                 }
-                _timeLabel.setText("Time: " + String.format("%02d",_minute) + ":" + String.format("%02d", _second));
             }
         });
+    }
+
+
+    public void startTimer() {
+        _timer.start();
+    }
+
+    public void pauseTimer() {
+        _paused = true;
+    }
+
+    public void resumeTimer() {
+        _paused = false;
+    }
+
+    public void restartTimer() {
+        _timer.restart();
     }
 
 }
