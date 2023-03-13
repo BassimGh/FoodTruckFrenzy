8779e052c51406ae453732a67a8b14155fbdd3c7
Patrick Shaw <patrickkshaw@gmail.com>
2023-03-13 00:44:02 -0700

    scoreboard UI updates

--- foodtruckfrenzy/src/main/java/foodtruckfrenzy/Scoreboard.java
+++ foodtruckfrenzy/src/main/java/foodtruckfrenzy/Scoreboard.java
@@ -2,18 +2,8 @@ package foodtruckfrenzy;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.GridLayout;
-import java.awt.FlowLayout;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.Timer;
-
-// TO DO
-// recipes collected panel
-// create progress bar or something to show how many items saved
-
+import java.awt.*;
+import javax.swing.*;
 
 public class Scoreboard extends JPanel {
 
@@ -24,25 +14,26 @@ public class Scoreboard extends JPanel {
     4. Time:                   records time elapsed
     5. Score:                  current score
     **/
-    Font infoFont = new Font("Arial", Font.BOLD, 18);
-    Font damageFont = new Font("Arial", Font.BOLD, 18);
+    Font smolBoiFont = new Font("Comic Sans MS", Font.PLAIN, 22);
+    Font bigBoiFont = new Font("Comic Sans MS", Font.BOLD, 40);
+    // Font smallerFont = new Font("Comic Sans MS", Font.PLAIN, 22);
     
     Color backGroundColor = new Color(54, 65, 79);
     
     private JPanel collectablesPanel = new JPanel();
     private JPanel negativePointsPanel = new JPanel();
     private JPanel scorePanel = new JPanel();
-    private JPanel clockPanel = new JPanel();
+    private JPanel timePanel = new JPanel();
     
     private JLabel _scoreLabel = new JLabel("Score: ");
     private JLabel _scoreTotal = new JLabel();
-    private JLabel _ingredientsLabel = new JLabel();
-    private JLabel _recipesLabel = new JLabel("Recipes Collected: ");
+    private JLabel _ingredientsLabel = new JLabel("Ingredients");
+    private JLabel _recipesLabel = new JLabel("Recipes: ");
     private JLabel _damageLabel = new JLabel("Damage: "); 
     private JLabel _fineLabel = new JLabel("Fines: ");
-    private JLabel _timeLabel = new JLabel();
+    private JLabel _timeLabel = new JLabel("Time: 00:00");
 
-    // private JLabel _pauseInstructions = new JLabel("Press 'P' to pause");
+    private JLabel _pauseInstructions = new JLabel("Press 'P' to pause");
 
     private Timer _timer;
     private int _minute;
@@ -58,51 +49,71 @@ public class Scoreboard extends JPanel {
     public Scoreboard(FoodTruck player) {
         
 
-        setLayout(new FlowLayout());
-
-        collectablesPanel.setLayout(new GridLayout(2, 1));
+        setBorder(BorderFactory.createLineBorder(Color.BLACK, 2)); // added border for debugging
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+ 
+        // left column-------- ingredients and recipess
+        collectablesPanel.setLayout(new GridLayout(2, 2, 25, 8));
         collectablesPanel.setBackground(backGroundColor);
-        _ingredientsLabel.setFont(infoFont);
+        _ingredientsLabel.setFont(smolBoiFont);
         _ingredientsLabel.setForeground(Color.WHITE);
-        _recipesLabel.setFont(infoFont);
+        _recipesLabel.setFont(smolBoiFont);
         _recipesLabel.setForeground(Color.WHITE);
+        //centre left--------- damage and fines
+        negativePointsPanel.setLayout(new GridLayout(2, 1, 25, 16));
+        negativePointsPanel.setBackground(backGroundColor);
+        _fineLabel.setFont(smolBoiFont);
+        _fineLabel.setForeground(Color.WHITE);
+        _timeLabel.setFont(smolBoiFont);
+        _timeLabel.setForeground(Color.WHITE); 
+        _damageLabel.setFont(smolBoiFont);
+        _damageLabel.setForeground(Color.WHITE);
+        // negativePointsPanel.add(_fineLabel);
+        // negativePointsPanel.add(_damageLabel);
         collectablesPanel.add(_ingredientsLabel);
+        collectablesPanel.add(_fineLabel);
         collectablesPanel.add(_recipesLabel);
+        collectablesPanel.add(_damageLabel);
 
+
+        
+        //Centre Column -------- Score
         scorePanel.setLayout(new FlowLayout());
         scorePanel.setBackground(backGroundColor);
-        _scoreLabel.setFont(infoFont);       
+        _scoreLabel.setFont(bigBoiFont);       
         _scoreLabel.setForeground(Color.WHITE);
-        _scoreTotal.setFont(infoFont);
-        _scoreTotal.setForeground(new Color(241, 215, 77)); // yellow
+        _scoreTotal.setFont(bigBoiFont);
+
+
+        // Right Column -------- Time / Pause Instr.
+        timePanel.setLayout(new GridLayout(2, 1, 10, 10));
+        timePanel.setBackground(backGroundColor);
+        _timeLabel.setFont(smolBoiFont);
+        _timeLabel.setForeground(Color.WHITE);
         scorePanel.add(_scoreLabel);
         scorePanel.add(_scoreTotal);
-
-        negativePointsPanel.setLayout(new GridLayout(2, 1));
-        negativePointsPanel.setBackground(backGroundColor);
-        _fineLabel.setFont(infoFont);
-        _fineLabel.setForeground(Color.WHITE);
-        _timeLabel.setFont(infoFont);
-        _timeLabel.setForeground(Color.WHITE); 
-        _damageLabel.setFont(damageFont);
-        _damageLabel.setForeground(Color.WHITE);
-        negativePointsPanel.add(_fineLabel);
-        negativePointsPanel.add(_damageLabel);
-
-        clockPanel.setLayout(new FlowLayout());
-        clockPanel.setBackground(backGroundColor);
-        _timeLabel.setFont(infoFont);
-        _timeLabel.setForeground(Color.RED);
-        // _pauseInstructions.setFont(font);
-        // _pauseInstructions.setForeground(Color.WHITE);
-        clockPanel.add(_timeLabel);
-        // clockPanel.add(_pauseInstructions);
+        timePanel.add(_timeLabel);
+        _pauseInstructions.setFont(smolBoiFont);
+        _pauseInstructions.setForeground(Color.WHITE);
+        timePanel.add(_pauseInstructions);
         
+        gbc.gridx = 0; // first column
+        gbc.gridy = 0; // first row
+        gbc.anchor = GridBagConstraints.WEST; // left align   
+        // gbc.insets = new Insets(10, 0, 10, 20); 
+        add(collectablesPanel, gbc);
 
-        add(collectablesPanel);
-        add(negativePointsPanel);
-        add(scorePanel);
-        add(clockPanel);
+        // add(negativePointsPanel);
+        gbc.gridx = 1; // second column
+        gbc.anchor = GridBagConstraints.CENTER; // centre align 
+        // gbc.insets = new Insets(10, 10, 10, 10);
+        add(scorePanel, gbc);
+
+        gbc.gridx = 2; // third column
+        gbc.anchor = GridBagConstraints.EAST; // right align
+        // gbc.insets = new Insets(10, 20, 10, 0);
+        add(timePanel, gbc);
 
         
         setBackground(backGroundColor);
@@ -116,12 +127,20 @@ public class Scoreboard extends JPanel {
 
     
     public void update() {
-
-        _ingredientsLabel.setText("Ingredients Collected: " + player.getIngredientsFound() + "/" + Food.getCount());
-        _recipesLabel.setText("Recipes Found: " + player.getRecipesFound() + "/" + Recipe.getCount());
-        _damageLabel.setText("Damages: " + player.getDamage());
-        _fineLabel.setText("Speed Fines: " + player.getFines());
-        _scoreTotal.setText(player.getScore());
+        
+        int score = player.getScoreInt();
+        if(score < 0) {
+            _scoreTotal.setForeground(Color.RED);
+        }
+        else {
+            _scoreTotal.setForeground(Color.YELLOW);
+        }
+            _ingredientsLabel.setText("Ingredients: " + player.getIngredientsFound() + "/" + Food.getCount());
+            _recipesLabel.setText("Recipes: " + player.getRecipesFound() + "/" + Recipe.getCount());
+            _damageLabel.setText("Damages: " + player.getDamage());
+            _fineLabel.setText("Speed Fines: " + player.getFines());
+            _scoreTotal.setText(player.getScore());
+      
     }
 
 
