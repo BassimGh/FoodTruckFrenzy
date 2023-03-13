4d49c16215c1b37949f110161672010f34a1a334
cpinca <cpinca@sfu.ca>
2023-03-13 02:05:42 -0700

    commented out code in Main to implement GameOver

--- foodtruckfrenzy/src/main/java/foodtruckfrenzy/Game.java
+++ foodtruckfrenzy/src/main/java/foodtruckfrenzy/Game.java
@@ -117,13 +117,11 @@ public class Game {
 
     public void startTimer() {
         _timer.start();
-        _scoreboardPanel.startTimer();
     }
 
     public void pause() {
         _keyboardHandler.resetKeys();
         _paused = true;
-        _scoreboardPanel.pauseTimer();
         _layout.show(_mainPanel, "pause");
 
         _gamePanel.setFocusable(true);
@@ -132,7 +130,6 @@ public class Game {
 
     public void resume() {
         _paused = false;
-        _scoreboardPanel.resumeTimer();
         _layout.show(_mainPanel, "game");
         _gamePanel.setFocusable(true);
         _gamePanel.requestFocusInWindow();
