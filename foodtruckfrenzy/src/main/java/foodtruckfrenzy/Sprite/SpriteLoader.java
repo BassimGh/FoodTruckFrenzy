d937d736a62cc236b7a9bc3e05e9dc9e47dbdd9c
Kyle Deliyannides <kkd16@sfu.ca>
2023-03-14 22:00:07 -0700

    update spriteloader to use enum map

--- foodtruckfrenzy/src/main/java/foodtruckfrenzy/Sprite/SpriteLoader.java
+++ foodtruckfrenzy/src/main/java/foodtruckfrenzy/Sprite/SpriteLoader.java
@@ -4,19 +4,19 @@ import foodtruckfrenzy.Drawable.DrawableEnum;
 import javax.swing.ImageIcon;
 import java.util.ArrayList;
 import java.util.Random;
-import java.util.HashMap;
+import java.util.EnumMap;
 
 /**
  * SpriteLoader preloads all the sprites for the Drawable objects at load time
  * This is used so the images are only loaded once to increase performance
- * The images are stored in a hash map and returned when getImage is called
+ * The images are stored in an enum map and returned when getImage is called
  */
 public class SpriteLoader {
 
     // house and food are ArrayLists because they have multiple images for the same object, selected at random
     private static ArrayList<ImageIcon> _houseImages = new ArrayList<ImageIcon>();
     private static ArrayList<ImageIcon> _foodImages = new ArrayList<ImageIcon>();
-    private static HashMap<DrawableEnum, ImageIcon> _imagesHashMap = new HashMap<>();
+    private static EnumMap<DrawableEnum, ImageIcon> _imagesEnumMap = new EnumMap<>(DrawableEnum.class);
 
     private static Random random = new Random();
 
@@ -24,20 +24,20 @@ public class SpriteLoader {
             
         try {
 
-            _imagesHashMap.put(DrawableEnum.FOODTRUCK_UP, new ImageIcon(SpriteLoader.class.getResource("foodTruckUp.png")));
-            _imagesHashMap.put(DrawableEnum.FOODTRUCK_DOWN, new ImageIcon(SpriteLoader.class.getResource("foodTruckDown.png")));
-            _imagesHashMap.put(DrawableEnum.FOODTRUCK_RIGHT, new ImageIcon(SpriteLoader.class.getResource("foodTruckRight.png")));
-            _imagesHashMap.put(DrawableEnum.FOODTRUCK_LEFT, new ImageIcon(SpriteLoader.class.getResource("foodTruckLeft.png")));
+            _imagesEnumMap.put(DrawableEnum.FOODTRUCK_UP, new ImageIcon(SpriteLoader.class.getResource("foodTruckUp.png")));
+            _imagesEnumMap.put(DrawableEnum.FOODTRUCK_DOWN, new ImageIcon(SpriteLoader.class.getResource("foodTruckDown.png")));
+            _imagesEnumMap.put(DrawableEnum.FOODTRUCK_RIGHT, new ImageIcon(SpriteLoader.class.getResource("foodTruckRight.png")));
+            _imagesEnumMap.put(DrawableEnum.FOODTRUCK_LEFT, new ImageIcon(SpriteLoader.class.getResource("foodTruckLeft.png")));
             
-            _imagesHashMap.put(DrawableEnum.COP_UP, new ImageIcon(SpriteLoader.class.getResource("copCarUp.gif")));
-            _imagesHashMap.put(DrawableEnum.COP_DOWN, new ImageIcon(SpriteLoader.class.getResource("copCarDown.gif")));
-            _imagesHashMap.put(DrawableEnum.COP_RIGHT, new ImageIcon(SpriteLoader.class.getResource("copCarRight.gif")));
-            _imagesHashMap.put(DrawableEnum.COP_LEFT, new ImageIcon(SpriteLoader.class.getResource("copCarLeft.gif")));
+            _imagesEnumMap.put(DrawableEnum.COP_UP, new ImageIcon(SpriteLoader.class.getResource("copCarUp.gif")));
+            _imagesEnumMap.put(DrawableEnum.COP_DOWN, new ImageIcon(SpriteLoader.class.getResource("copCarDown.gif")));
+            _imagesEnumMap.put(DrawableEnum.COP_RIGHT, new ImageIcon(SpriteLoader.class.getResource("copCarRight.gif")));
+            _imagesEnumMap.put(DrawableEnum.COP_LEFT, new ImageIcon(SpriteLoader.class.getResource("copCarLeft.gif")));
 
-            _imagesHashMap.put(DrawableEnum.SPEED_TRAP, new ImageIcon(SpriteLoader.class.getResource("speedtrap.png")));
-            _imagesHashMap.put(DrawableEnum.POT_HOLE, new ImageIcon(SpriteLoader.class.getResource("pothole.png")));
-            _imagesHashMap.put(DrawableEnum.RECIPE, new ImageIcon(SpriteLoader.class.getResource("recipe.png")));
-            _imagesHashMap.put(DrawableEnum.PICKUP_GLITTER, new ImageIcon(SpriteLoader.class.getResource("pickupGlitter.gif")));
+            _imagesEnumMap.put(DrawableEnum.SPEED_TRAP, new ImageIcon(SpriteLoader.class.getResource("speedtrap.png")));
+            _imagesEnumMap.put(DrawableEnum.POT_HOLE, new ImageIcon(SpriteLoader.class.getResource("pothole.png")));
+            _imagesEnumMap.put(DrawableEnum.RECIPE, new ImageIcon(SpriteLoader.class.getResource("recipe.png")));
+            _imagesEnumMap.put(DrawableEnum.PICKUP_GLITTER, new ImageIcon(SpriteLoader.class.getResource("pickupGlitter.gif")));
 
             _houseImages.add(new ImageIcon(SpriteLoader.class.getResource("house1.png")));
             _houseImages.add(new ImageIcon(SpriteLoader.class.getResource("house2.png")));
@@ -55,19 +55,19 @@ public class SpriteLoader {
             _foodImages.add(new ImageIcon(SpriteLoader.class.getResource("egg.png")));
             _foodImages.add(new ImageIcon(SpriteLoader.class.getResource("can.png")));
             
-            _imagesHashMap.put(DrawableEnum.HORIZONTAL_ROAD, new ImageIcon(SpriteLoader.class.getResource("road/Horizontal.png")));
-            _imagesHashMap.put(DrawableEnum.VERTICAL_ROAD, new ImageIcon(SpriteLoader.class.getResource("road/Vertical.png")));
-            _imagesHashMap.put(DrawableEnum.FOUR_WAY_INTERSECT, new ImageIcon(SpriteLoader.class.getResource("road/FourWay.png")));
+            _imagesEnumMap.put(DrawableEnum.HORIZONTAL_ROAD, new ImageIcon(SpriteLoader.class.getResource("road/Horizontal.png")));
+            _imagesEnumMap.put(DrawableEnum.VERTICAL_ROAD, new ImageIcon(SpriteLoader.class.getResource("road/Vertical.png")));
+            _imagesEnumMap.put(DrawableEnum.FOUR_WAY_INTERSECT, new ImageIcon(SpriteLoader.class.getResource("road/FourWay.png")));
 
-            _imagesHashMap.put(DrawableEnum.SOUTH_EAST_CORNER, new ImageIcon(SpriteLoader.class.getResource("road/SouthEastCorner.png")));
-            _imagesHashMap.put(DrawableEnum.SOUTH_WEST_CORNER, new ImageIcon(SpriteLoader.class.getResource("road/SouthWestCorner.png")));
-            _imagesHashMap.put(DrawableEnum.NORTH_EAST_CORNER, new ImageIcon(SpriteLoader.class.getResource("road/NorthEastCorner.png")));
-            _imagesHashMap.put(DrawableEnum.NORTH_WEST_CORNER, new ImageIcon(SpriteLoader.class.getResource("road/NorthWestCorner.png")));
+            _imagesEnumMap.put(DrawableEnum.SOUTH_EAST_CORNER, new ImageIcon(SpriteLoader.class.getResource("road/SouthEastCorner.png")));
+            _imagesEnumMap.put(DrawableEnum.SOUTH_WEST_CORNER, new ImageIcon(SpriteLoader.class.getResource("road/SouthWestCorner.png")));
+            _imagesEnumMap.put(DrawableEnum.NORTH_EAST_CORNER, new ImageIcon(SpriteLoader.class.getResource("road/NorthEastCorner.png")));
+            _imagesEnumMap.put(DrawableEnum.NORTH_WEST_CORNER, new ImageIcon(SpriteLoader.class.getResource("road/NorthWestCorner.png")));
             
-            _imagesHashMap.put(DrawableEnum.THREE_WAY_NORTH, new ImageIcon(SpriteLoader.class.getResource("road/ThreeWayNorth.png")));
-            _imagesHashMap.put(DrawableEnum.THREE_WAY_EAST, new ImageIcon(SpriteLoader.class.getResource("road/ThreeWayEast.png")));
-            _imagesHashMap.put(DrawableEnum.THREE_WAY_SOUTH, new ImageIcon(SpriteLoader.class.getResource("road/ThreeWaySouth.png")));
-            _imagesHashMap.put(DrawableEnum.THREE_WAY_WEST, new ImageIcon(SpriteLoader.class.getResource("road/ThreeWayWest.png")));
+            _imagesEnumMap.put(DrawableEnum.THREE_WAY_NORTH, new ImageIcon(SpriteLoader.class.getResource("road/ThreeWayNorth.png")));
+            _imagesEnumMap.put(DrawableEnum.THREE_WAY_EAST, new ImageIcon(SpriteLoader.class.getResource("road/ThreeWayEast.png")));
+            _imagesEnumMap.put(DrawableEnum.THREE_WAY_SOUTH, new ImageIcon(SpriteLoader.class.getResource("road/ThreeWaySouth.png")));
+            _imagesEnumMap.put(DrawableEnum.THREE_WAY_WEST, new ImageIcon(SpriteLoader.class.getResource("road/ThreeWayWest.png")));
         } catch (Exception e) {
             System.err.println("Error when loading sprite files with SpriteLoader:");
             e.printStackTrace();
@@ -75,7 +75,7 @@ public class SpriteLoader {
     }
 
     /**
-     * Returns the appropriate sprite image requested from the hash map
+     * Returns the appropriate sprite image requested from the enum map
      * or randomly from either the house or food ArrayLists if applicable
      * @param type DrawableEnum type of Drawable sprite that we need the image for
      * @return The appropriate Image as requested
@@ -90,6 +90,6 @@ public class SpriteLoader {
             return _foodImages.get(randomNumber);
         }
     
-        return _imagesHashMap.get(type);
+        return _imagesEnumMap.get(type);
     }
 }
