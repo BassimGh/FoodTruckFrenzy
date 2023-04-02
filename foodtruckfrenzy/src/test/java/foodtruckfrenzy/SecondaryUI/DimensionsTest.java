0bd1903d64a2ed2d71d58be7011a47cb386beaec
cpinca <cpinca@sfu.ca>
2023-04-01 18:21:46 -0700

    created test classes and implemented tests

---
+++ foodtruckfrenzy/src/test/java/foodtruckfrenzy/SecondaryUI/DimensionsTest.java
@@ -0,0 +1,36 @@
+package foodtruckfrenzy.SecondaryUI;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DimensionsTest {
+    private Dimensions dimensions;
+
+    @Before
+    public void setUp() {
+        dimensions = new Dimensions(10,15);
+    }
+
+    @Test
+    public void testGetHeight() {
+        int actualHeight = dimensions.getHeight();
+        int expectedHeight = 15;
+        assertEquals(expectedHeight, actualHeight);
+    }
+
+    @Test
+    public void testGetWidth() {
+        int actualWidth = dimensions.getWidth();
+        int expectedWidth = 10;
+        assertEquals(expectedWidth, actualWidth);
+    }
+
+    @Test
+    public void testConstructor() {
+        Dimensions dim = new Dimensions(2,3);
+        assertNotNull(dim);
+    }
+}
--- foodtruckfrenzy/src/test/java/foodtruckfrenzy/SecondaryUI/ImagePathsTest.java
+++ foodtruckfrenzy/src/test/java/foodtruckfrenzy/SecondaryUI/ImagePathsTest.java
@@ -1,20 +1,43 @@
 package foodtruckfrenzy.SecondaryUI;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class ImagePathsTest {
+    private ImagePaths imagePaths;
+
+    @Before
+    public void setUp() {
+        imagePaths = new ImagePaths("backgroundPath", "button1path", "button2path");
+    }
+
     @Test
     public void testGetBackgroundPath() {
-
+        String actualPath = imagePaths.getBackgroundPath();
+        String expectedPath = "backgroundPath";
+        assertEquals(expectedPath, actualPath);
     }
 
     @Test
     public void testGetButton1Path() {
-
+        String actualPath = imagePaths.getButton1Path();
+        String expectedPath = "button1path";
+        assertEquals(expectedPath, actualPath);
     }
 
     @Test
     public void testGetButton2Path() {
+        String actualPath = imagePaths.getButton2Path();
+        String expectedPath = "button2path";
+        assertEquals(expectedPath, actualPath);
+    }
 
+    @Test
+    public void testConstructor() {
+        ImagePaths paths = new ImagePaths("background.png", "button1.png", "button2.png");
+        assertNotNull(paths);
     }
 }
