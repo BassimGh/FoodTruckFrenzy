3958e6c7b5699d41ab91e3e1bd71db98c9a18ce9
Kyle Deliyannides <kkd16@sfu.ca>
2023-04-02 16:44:21 -0700

    add a test map layout object init

--- foodtruckfrenzy/src/test/java/foodtruckfrenzy/Helper/MapLayoutTest.java
+++ foodtruckfrenzy/src/test/java/foodtruckfrenzy/Helper/MapLayoutTest.java
@@ -7,15 +7,15 @@ import foodtruckfrenzy.GameFramework.Grid;
 
 public class MapLayoutTest {
     
-    private MapLayout mapLayout;
-
-    @BeforeEach
-    void setUp() {
-        mapLayout = new MapLayout();
+    @Test
+    void testMapLayoutObjectCreation() {
+        MapLayout mapLayout = new MapLayout();
+        assertNotNull(mapLayout, "Failed to initialize MapLayout");
     }
 
     @Test
     void testGetElementAtEachNotNull() {
+        MapLayout mapLayout = new MapLayout();
         for (int row = 0; row < Grid.ROWS; row++) {
             for (int col = 0; col < Grid.COLS; col++) {
                 LayoutEnum element = mapLayout.getElementAt(row, col);
