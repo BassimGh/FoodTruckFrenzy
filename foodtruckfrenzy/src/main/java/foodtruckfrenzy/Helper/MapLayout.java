3ea5d552e7ea13a2cb94b8779c413fd916d65616
Kyle Deliyannides <kkd16@sfu.ca>
2023-04-02 17:02:59 -0700

    add error handling to MapLayout

--- foodtruckfrenzy/src/main/java/foodtruckfrenzy/Helper/MapLayout.java
+++ foodtruckfrenzy/src/main/java/foodtruckfrenzy/Helper/MapLayout.java
@@ -16,18 +16,38 @@ public class MapLayout {
 
     public MapLayout() {
         InputStream inputStream = MapLayout.class.getResourceAsStream("layout.csv");
-        Scanner scanner = new Scanner(inputStream);
-        layout = new LayoutEnum[Grid.ROWS][Grid.COLS];
-        int row = 0;
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            String[] items = line.split(",");
-            for (int col = 0; col < items.length; col++) {
-                layout[row][col] = LayoutEnum.valueOf(items[col]);
-            }
-            row++;
+        if (inputStream == null) {
+            throw new RuntimeException("Unable to open layout.csv file input stream.");
         }
-        scanner.close();
+
+        try (Scanner scanner = new Scanner(inputStream)) {
+            layout = new LayoutEnum[Grid.ROWS][Grid.COLS];
+            int row = 0;
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] items = line.split(",");
+                
+                if (items.length != Grid.COLS) {
+                    throw new RuntimeException("The number of columns in the layout.csv file do not match Grid.cols");
+                }
+
+                for (int col = 0; col < items.length; col++) {
+
+                    try {
+                        layout[row][col] = LayoutEnum.valueOf(items[col]);
+                    } catch (IllegalArgumentException iae) {
+                        throw new RuntimeException("Invalid LayoutEnum valie in layout.csv file at row " + row + ", col " + col, iae);
+                    }
+                    
+                }
+                row++;
+            }
+
+            if (row != Grid.ROWS) {
+                throw new RuntimeException("The number of rows in the layout.csv file does not match Grid.ROWS.");
+            }
+            scanner.close();
+        }
     }
 
     /**
