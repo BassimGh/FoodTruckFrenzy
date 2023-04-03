71e97967cca13f0aaebee2ba29e87184f6c2e49f
bga32 <bga32@sfu.ca>
2023-04-02 19:12:18 -0700

    added vehicleSpawner unit tests

---
+++ foodtruckfrenzy/src/test/java/foodtruckfrenzy/Helper/VehicleSpawnerTest.java
@@ -0,0 +1,37 @@
+package foodtruckfrenzy.Helper;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.*;
+
+import foodtruckfrenzy.Drawable.Vehicle.Cop;
+import foodtruckfrenzy.Drawable.Vehicle.FoodTruck;
+import foodtruckfrenzy.GameFramework.Grid;
+
+public class VehicleSpawnerTest {
+    VehicleSpawner vehicleSpawner;
+    Grid grid;
+
+    @BeforeEach
+    void setup() {
+        grid = new Grid();
+        vehicleSpawner = new VehicleSpawner(grid);
+    }
+
+    @Test
+    void testGetFoodTruck() {
+        FoodTruck foodTruck = vehicleSpawner.getFoodTruck();
+        assertTrue(foodTruck instanceof FoodTruck);
+        assertNotNull(foodTruck);
+    }
+    @Test
+    void testGetCops() {
+        ArrayList<Cop> cops = vehicleSpawner.getCops();
+        for(Cop cop : cops){
+            assertTrue(cop instanceof Cop);
+            assertNotNull(cop);
+        }
+    }
+}
