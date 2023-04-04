1ddca9ea16f190469c1e602b00a61e29d87c5ab7
Kyle Deliyannides <kkd16@sfu.ca>
2023-04-03 17:16:11 -0700

    improve isobsrtruction tests

--- foodtruckfrenzy/src/test/java/foodtruckfrenzy/GameFramework/GridTest.java
+++ foodtruckfrenzy/src/test/java/foodtruckfrenzy/GameFramework/GridTest.java
@@ -6,9 +6,9 @@ import org.junit.jupiter.api.*;
 import org.mockito.Mockito;
 
 import foodtruckfrenzy.Drawable.BoardElement.BoardElement;
-import foodtruckfrenzy.Drawable.BoardElement.Obstruction;
 import foodtruckfrenzy.Drawable.BoardElement.Road;
 import foodtruckfrenzy.Helper.BoardElementFactory;
+import foodtruckfrenzy.Helper.LayoutEnum;
 import foodtruckfrenzy.Helper.MapLayout;
 
 class GridTest {
@@ -30,14 +30,13 @@ class GridTest {
 
     @Test
     public void testIsObstructionTrue() {
-        BoardElementFactory boardElementFactory = Mockito.mock(BoardElementFactory.class);
+        BoardElementFactory boardElementFactory = new BoardElementFactory();
         MapLayout mapLayout = Mockito.mock(MapLayout.class);
-        BoardElement mockElement = Mockito.mock(Obstruction.class);
-        Mockito.when(boardElementFactory.create(Mockito.any(), Mockito.anyInt(), Mockito.anyInt())).thenReturn(mockElement);
+        Mockito.when(mapLayout.getElementAt(Mockito.anyInt(), Mockito.anyInt())).thenReturn(LayoutEnum.O);
         Grid grid = new Grid(boardElementFactory, mapLayout);
-
-        for (int i = 0; i<Grid.ROWS; i++) {
-            for (int j = 0; j<Grid.COLS; j++) {
+    
+        for (int i = 0; i < Grid.ROWS; i++) {
+            for (int j = 0; j < Grid.COLS; j++) {
                 assertTrue(grid.isObstruction(i, j));
             }
         }
@@ -45,14 +44,13 @@ class GridTest {
 
     @Test
     public void testIsObstructionFalse() {
-        BoardElementFactory boardElementFactory = Mockito.mock(BoardElementFactory.class);
+        BoardElementFactory boardElementFactory = new BoardElementFactory();
         MapLayout mapLayout = Mockito.mock(MapLayout.class);
-        BoardElement mockElement = Mockito.mock(Road.class);
-        Mockito.when(boardElementFactory.create(Mockito.any(), Mockito.anyInt(), Mockito.anyInt())).thenReturn(mockElement);
+        Mockito.when(mapLayout.getElementAt(Mockito.anyInt(), Mockito.anyInt())).thenReturn(LayoutEnum.R);
         Grid grid = new Grid(boardElementFactory, mapLayout);
-
-        for (int i = 0; i<Grid.ROWS; i++) {
-            for (int j = 0; j<Grid.COLS; j++) {
+    
+        for (int i = 0; i < Grid.ROWS; i++) {
+            for (int j = 0; j < Grid.COLS; j++) {
                 assertFalse(grid.isObstruction(i, j));
             }
         }
@@ -60,6 +58,17 @@ class GridTest {
 
     @Test
     public void testIngredientsDiscoverableZero() {
+        BoardElementFactory boardElementFactory = Mockito.mock(BoardElementFactory.class);
+        MapLayout mapLayout = Mockito.mock(MapLayout.class);
+        BoardElement mockElement = Mockito.mock(Road.class);
+        Mockito.when(boardElementFactory.create(Mockito.any(), Mockito.anyInt(), Mockito.anyInt())).thenReturn(mockElement);
+        Grid grid = new Grid(boardElementFactory, mapLayout);
+
+        assertEquals(0, grid.getIngredientsDiscoverable());
+    }
+
+    @Test
+    public void testIngredientsDiscoverableMax() {
         BoardElementFactory boardElementFactory = Mockito.mock(BoardElementFactory.class);
         MapLayout mapLayout = Mockito.mock(MapLayout.class);
         BoardElement mockElement = Mockito.mock(Road.class);
