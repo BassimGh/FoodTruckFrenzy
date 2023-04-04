d9da0379e53b2b61dfa5f5a2cfd3e38e278a65bc
Kyle Deliyannides <kkd16@sfu.ca>
2023-04-03 18:02:33 -0700

    check win true

--- foodtruckfrenzy/src/test/java/foodtruckfrenzy/GameFramework/GameConditionsTest.java
+++ foodtruckfrenzy/src/test/java/foodtruckfrenzy/GameFramework/GameConditionsTest.java
@@ -10,15 +10,17 @@ import foodtruckfrenzy.Drawable.Vehicle.Cop;
 import foodtruckfrenzy.Drawable.Vehicle.FoodTruck;
 
 public class GameConditionsTest {
-    
+
+    private final int MAX_INGREDIENTS = 5;
     private GameConditions gameConditions;
     private Scoreboard scoreboard;
     private FoodTruck foodTruck;
     private ArrayList<Cop> cops;
 
+
     @BeforeEach
     void setup() {
-        scoreboard = new Scoreboard(5, 5);
+        scoreboard = new Scoreboard(MAX_INGREDIENTS, 0);
         foodTruck = new FoodTruck(5, 6, null, scoreboard);
         cops = new ArrayList<>();
         gameConditions = new GameConditions(cops, foodTruck);
@@ -56,6 +58,18 @@ public class GameConditionsTest {
         assertTrue(gameConditions.checkLoss());
         cops.clear();
         assertFalse(gameConditions.checkLoss());
+    }
+
+    @Test
+    void checkWinTrue() {
+        foodTruck.setCol(40);
+        foodTruck.setRow(16);
+        for( int i = 0; i < MAX_INGREDIENTS; i++) {
+            scoreboard.addIngredientsFound();
+        }
+
+        assertTrue(gameConditions.checkWin());
+
     }
 
 }
