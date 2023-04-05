99f3f11fa12455497aadc019f065f0b55ad27b7c
Kyle Deliyannides <kkd16@sfu.ca>
2023-04-04 18:32:09 -0700

    create game frame getters

---
+++ foodtruckfrenzy/src/test/java/foodtruckfrenzy/GameFramework/GameFrameTest.java
@@ -0,0 +1,73 @@
+package foodtruckfrenzy.GameFramework;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import javax.swing.JPanel;
+import java.awt.Component;
+import org.junit.jupiter.api.*;
+import org.mockito.Mockito;
+
+import foodtruckfrenzy.SecondaryUI.PauseScreen;
+
+public class GameFrameTest {
+    private GamePanel gamePanel;
+    private Scoreboard scoreboardPanel;
+    private PauseScreen pauseScreen;
+    private GameFrame gameFrame;
+
+
+    @BeforeEach
+    void setup() {
+        gamePanel = spy(new GamePanel(null, null, null));
+        scoreboardPanel = new Scoreboard(0, 0);
+        pauseScreen = new PauseScreen(null, null);
+        gameFrame = new GameFrame(gamePanel, scoreboardPanel, pauseScreen);
+    }
+
+    @AfterEach
+    void close() {
+        gameFrame.dispose();
+    }
+
+    @Test
+    void testGameFrameCreation() {
+        assertNotNull(gameFrame);
+        assertEquals(gamePanel, gameFrame.getGamePanel());
+        assertTrue(gameFrame.getMainPanel().getComponent(0) instanceof JPanel);
+        assertEquals(pauseScreen, gameFrame.getMainPanel().getComponent(1));
+    }
+
+    @Test
+    void testShowPauseScreen() {
+        gameFrame.showPauseScreen();
+        Component visibleComponent = gameFrame.getMainPanel().getComponents()[getVisibleComponentIndex(gameFrame.getMainPanel())];
+        assertEquals(pauseScreen, visibleComponent);
+    }
+
+    @Test
+    void testShowGameScreen() {
+        gameFrame.showGameScreen();
+        Component visibleComponent = gameFrame.getMainPanel().getComponents()[getVisibleComponentIndex(gameFrame.getMainPanel())];
+        assertEquals(gameFrame.getGamePanel().getParent(), visibleComponent);
+    }
+
+    @Test
+    void testRefresh() {
+        GameFrame gameFrame = new GameFrame(gamePanel, scoreboardPanel, pauseScreen);
+        Mockito.reset(gamePanel);
+        gameFrame.refresh();
+        verify(gamePanel, times(1)).repaint();
+}
+
+    int getVisibleComponentIndex(JPanel mainPanel) {
+        for (int i = 0; i < mainPanel.getComponentCount(); i++) {
+            if (mainPanel.getComponent(i).isVisible()) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
