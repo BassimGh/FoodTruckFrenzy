b03590d1cd3aac76d9ce420527acc5d92dd67b88
Kyle Deliyannides <kkd16@sfu.ca>
2023-04-04 21:36:34 -0700

    add some mockito game loop tests, win loss

--- foodtruckfrenzy/src/test/java/foodtruckfrenzy/GameFramework/GameTest.java
+++ foodtruckfrenzy/src/test/java/foodtruckfrenzy/GameFramework/GameTest.java
@@ -1,13 +1,19 @@
 package foodtruckfrenzy.GameFramework;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 import org.junit.jupiter.api.*;
+import org.mockito.Mockito;
+
+import foodtruckfrenzy.Helper.KeyboardHandler;
 
 public class GameTest {
     private Game game;
 
+
     @BeforeEach
     public void setup() {
-        game = new Game();
+        game = spy(new Game());
     }
 
     @AfterEach
@@ -29,4 +35,54 @@ public class GameTest {
         game.handleResume();
         assertTrue(game.getGameConditions().checkRunning());
     }
+
+    @Test
+    void testPausePressed() {
+        KeyboardHandler keyboardHandler = Mockito.mock(KeyboardHandler.class);
+        when(keyboardHandler.pausePressed()).thenReturn(true);
+
+        game.overrideKeyboardHandler(keyboardHandler);
+        game.gameTick();
+
+        verify(game, times(1)).handlePause();
+    }
+
+    @Test
+    void testLossConditionTrueOne() {
+        GameConditions gameCondtions = Mockito.mock(GameConditions.class);
+        when(gameCondtions.checkLoss()).thenReturn(true);
+        when(gameCondtions.checkRunning()).thenReturn(true);
+
+        game.overrideGameConditions(gameCondtions);
+        game.gameTick();
+
+        verify(game, times(1)).handleLoss();
+    }
+
+    @Test
+    void testLossConditionTrueTwo() {
+        GameConditions gameCondtions = Mockito.mock(GameConditions.class);
+        when(gameCondtions.checkLoss()).thenReturn(false).thenReturn(true);
+        when(gameCondtions.checkRunning()).thenReturn(true);
+
+        game.overrideGameConditions(gameCondtions);
+        game.gameTick();
+
+        verify(game, times(1)).handleLoss();
+    }
+
+    @Test
+    void testWinConditionTrue() {
+        GameConditions gameCondtions = Mockito.mock(GameConditions.class);
+        when(gameCondtions.checkWin()).thenReturn(true);
+        when(gameCondtions.checkRunning()).thenReturn(true);
+
+        game.overrideGameConditions(gameCondtions);
+        game.gameTick();
+
+        verify(game, times(1)).handleWin();
+    }
+
+
+
 }
