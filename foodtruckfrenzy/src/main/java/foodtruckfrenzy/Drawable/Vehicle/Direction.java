239d82ea0666a10e34f9a6aa60958ef7fdde2e4f
Kyle Deliyannides <kkd16@sfu.ca>
2023-04-10 18:52:00 -0700

    doc direction

--- foodtruckfrenzy/src/main/java/foodtruckfrenzy/Drawable/Vehicle/Direction.java
+++ foodtruckfrenzy/src/main/java/foodtruckfrenzy/Drawable/Vehicle/Direction.java
@@ -1,11 +1,27 @@
 package foodtruckfrenzy.Drawable.Vehicle;
 
 /**
- * Enum containing the possible Directions a Vehicle can move to
+ * Enum containing the possible Directions a Vehicle can move to.
  */
 public enum Direction {
+
+    /**
+     * Up vehicle direction.
+     */
     UP,
+
+    /**
+     * Down vehicle direction.
+     */
     DOWN,
+
+    /**
+     * Left vehicle direction.
+     */
     LEFT,
+
+    /**
+     * Right vehicle direction.
+     */
     RIGHT
 }
